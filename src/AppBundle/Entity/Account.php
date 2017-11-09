b00b2e79e29dd7155932f67412326c3ad69212ee
Jake Simpson <jake.t23@me.com>
2017-11-09 13:55:16 +0000

    Inital Commit 	new file:   .gitignore 	new file:   app/.htaccess 	new file:   app/AppCache.php 	new file:   app/AppKernel.php 	new file:   app/Resources/views/base.html.twig 	new file:   app/Resources/views/default/index.html.twig 	new file:   app/Resources/views/security/login.html.twig 	new file:   app/config/config.yml 	new file:   app/config/config_dev.yml 	new file:   app/config/config_prod.yml 	new file:   app/config/config_test.yml 	new file:   app/config/parameters.yml.dist 	new file:   app/config/routing.yml 	new file:   app/config/routing_dev.yml 	new file:   app/config/security.yml 	new file:   app/config/services.yml 	new file:   app/test.db 	new file:   bin/console 	new file:   bin/symfony_requirements 	new file:   composer.json 	new file:   composer.lock 	new file:   phpunit.xml.dist 	new file:   src/.htaccess 	new file:   src/AppBundle/AppBundle.php 	new file:   src/AppBundle/Controller/DefaultController.php 	new file:   src/AppBundle/Controller/SecurityController.php 	new file:   src/AppBundle/Entity/Account.php 	new file:   src/AppBundle/Entity/User.php 	new file:   src/AppBundle/Resources/config/doctrine/Account.orm.xml 	new file:   src/AppBundle/Resources/config/doctrine/User.orm.xml 	new file:   tests/AppBundle/Controller/DefaultControllerTest.php 	new file:   var/SymfonyRequirements.php 	new file:   var/cache/.gitkeep 	new file:   var/logs/.gitkeep 	new file:   var/sessions/.gitkeep 	new file:   web/.htaccess 	new file:   web/app.php 	new file:   web/app_dev.php 	new file:   web/apple-touch-icon.png 	new file:   web/config.php 	new file:   web/favicon.ico 	new file:   web/robots.txt

---
+++ src/AppBundle/Entity/Account.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Account
+ *
+ * @ORM\Table(name="account")
+ * @ORM\Entity
+ */
+class Account
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="userId", type="integer", nullable=true)
+     */
+    private $userid;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="text", nullable=true)
+     */
+    private $name;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+
+}
+
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity
+ */
+class User
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="text", nullable=true)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="text", nullable=true)
+     */
+    private $password;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function getSalt()
+    {
+        // you *may* need a real salt depending on your encoder
+        // see section on salt below
+        return FOaflFaD;
+    }
+
+    public function getRoles()
+    {
+        return array('ROLE_USER');
+    }
+
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->password,
+            // see section on salt below
+            // $this->salt,
+        ));
+    }
+
+    /** @see \Serializable::unserialize() */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password,
+            // see section on salt below
+            // $this->salt
+        ) = unserialize($serialized);
+    }
+}
+
